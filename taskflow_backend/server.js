f8474a5ac3fe06e0bfe4bfccc93dc3d6f02c8c43
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2026-02-18 13:47:57 +0530

    implement MVC folder structure with app separation

--- taskflow_backend/server.js
+++ taskflow_backend/server.js
@@ -1,10 +1,4 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
+const app = require('./src/app');
 
 const PORT = 5000;
 
