926303b443309ee2e7012f3740e5c01e9e2cd6bf
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2026-02-23 12:43:47 +0530

    made login page

--- taskflow-frontend/src/pages/Login.jsx
+++ taskflow-frontend/src/pages/Login.jsx
@@ -1,5 +1,41 @@
+import { useState, useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+
 function Login() {
-  return <h2>Login Page</h2>;
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await login(form);
+      toast.success('Logged in');
+      navigate('/');
+    } catch (error) {
+      toast.error(error.response?.data?.message || 'Invalid credentials');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input name="email" placeholder="Email" onChange={handleChange} />
+      <input name="password" type="password" placeholder="Password" onChange={handleChange} />
+      <button type="submit">Login</button>
+    </form>
+  );
 }
 
 export default Login;
