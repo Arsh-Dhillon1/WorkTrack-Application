f47b547c782f13780a594368c3364517ed3b19cd
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2026-02-23 11:37:11 +0530

    add task service

---
+++ taskflow_backend/src/services/taskService.js
@@ -0,0 +1,33 @@
+const Task = require('../models/Task');
+
+const createTask = async (data, userId) => {
+  const task = await Task.create({
+    ...data,
+    owner: userId
+  });
+
+  return task;
+};
+
+const getProjectTasks = async (projectId, userId) => {
+  return await Task.find({
+    project: projectId,
+    owner: userId
+  });
+};
+
+const updateTask = async (taskId, data, userId) => {
+  const task = await Task.findOneAndUpdate(
+    { _id: taskId, owner: userId },
+    data,
+    { new: true }
+  );
+
+  return task;
+};
+
+module.exports = {
+  createTask,
+  getProjectTasks,
+  updateTask
+};
